2f2130773c80297427fa5871e59083032246d4fa
Alessandro Tognon <sephir80@gmail.com>
2022-04-25 18:16:48 +0200

    [fix] random ball and random spawn time

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,22 +11,29 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        float spawnInterval;
+
+        spawnInterval = Random.Range(3.0f,5.0f);
+ 
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        int arrayBall;
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        arrayBall = Random.Range(0,ballPrefabs.Length);
+
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[arrayBall], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
 }
