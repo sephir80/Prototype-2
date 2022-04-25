e1c434cc204238fd48e1480902e98f523eda4f2d
Alessandro Tognon <sephir80@gmail.com>
2022-04-25 18:15:18 +0200

    [feature] add fireRate time

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-
+    public float sendDogRate=2.0f;
+    private float timeElapsed=0f;
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+
+         // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space)&&(timeElapsed<=0))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeElapsed=sendDogRate;
         }
+        timeElapsed-=Time.deltaTime;
+        Debug.Log("time elapsed = "+timeElapsed.ToString());
     }
+
 }
